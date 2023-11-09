47819a0373293913eef8633715b0c86d22339464
Petar Georgiev <petiyo07@abv.bg>
2023-11-10 00:26:49 +0200

    feat(Navigation): added errorPage and some func

--- router/src/components/Navigation/Navigation.js
+++ router/src/components/Navigation/Navigation.js
@@ -1,20 +1,35 @@
 import { NavLink, Outlet } from "react-router-dom";
-import classes from "./Navigation.module.css"
+import classes from "./Navigation.module.css";
 
 const Navigation = () => {
-    return (
-      <>
-        <ul className={classes.navLayout}>
-          <li>
-            <NavLink to={"/"}>Home</NavLink>
-          </li>
-          <li>
-            <NavLink to={"/products"}>Products</NavLink>
-          </li>
-            </ul>
-         <Outlet />
-      </>
-    );
+  return (
+    <>
+      <ul className={classes.navLayout}>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? classes.active : undefined
+            }
+            end
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/products"
+            className={({ isActive }) =>
+              isActive ? classes.active : undefined
+            }
+          >
+            Products
+          </NavLink>
+        </li>
+      </ul>
+      <Outlet />
+    </>
+  );
 };
 
 export default Navigation;
---
+++ router/src/pages/ErrorPage.js
@@ -0,0 +1,5 @@
+function ErrorPage() {
+    return <h1> Oops! An error occured</h1>
+}
+
+export default ErrorPage
